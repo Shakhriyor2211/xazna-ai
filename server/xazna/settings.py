bae76b7bade60bf10e0bfbb8c5b4f1fceeefeafd
shakhriyor2211 <shakhriyormamadaliev@gmail.com>
2025-11-07 11:49:19 +0500

    fix

--- server/xazna/settings.py
+++ server/xazna/settings.py
@@ -254,6 +254,8 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
+APPEND_SLASH = False
+
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_HOST_USER = os.getenv("SMTP_HOST_USER")
