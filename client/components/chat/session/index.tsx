416b001a77f2fda640c1f313e0b0079c81d27126
shakhriyor2211 <shakhriyormamadaliev@gmail.com>
2025-11-13 16:47:08 +0500

    fix

--- client/components/chat/session/index.tsx
+++ client/components/chat/session/index.tsx
@@ -85,7 +85,7 @@ export function ChatSession() {
     if (ws.current !== null) ws.current.close();
 
     ws.current = new WebSocket(
-      `ws:localhost:8000${ENDPOINTS.ws_client_base}/${ENDPOINTS.chat_message}/${sessionId}`
+      `${ENDPOINTS.ws_client_base}/${ENDPOINTS.chat_message}/${sessionId}`
     );
 
     ws.current.onmessage = async (event) => {
