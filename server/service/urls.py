832d4a084fa237946b5eccc22323caaca0e2e0d6
shakhriyor2211 <shakhriyormamadaliev@gmail.com>
2025-12-01 16:19:38 +0500

    fix

--- server/service/urls.py
+++ server/service/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path("token/item/<uuid:token_id>", ServiceTokenItemView.as_view(), name="service_token_item"),
     path("token/list", ServiceTokenListView.as_view(), name="service_token_list"),
     path("token/key/<uuid:token_id>", ServiceTokenKeyView.as_view(), name="service_token_key"),
-    path("token/manage/<uuid:token_id>", ServiceTokenManageView.as_view(), name="service_token_key"),
+    path("token/manage/<uuid:token_id>", ServiceTokenManageView.as_view(), name="service_token_manage"),
 ]
