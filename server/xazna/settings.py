d766b0bb727a9dbb0228d209597366a1ca7536c9
shakhriyor2211 <shakhriyormamadaliev@gmail.com>
2025-12-02 14:54:34 +0500

    fix

--- server/xazna/settings.py
+++ server/xazna/settings.py
@@ -68,12 +68,7 @@ GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
 GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
 GOOGLE_REDIRECT_URI = os.getenv("GOOGLE_REDIRECT_URI")
 
-ALLOWED_HOSTS = [
-    "localhost",
-    "127.0.0.1",
-    "0.0.0.0",
-    "ai.xazna.uz"
-]
+ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
