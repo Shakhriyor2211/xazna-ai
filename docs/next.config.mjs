99f5412124ce97c61fd7a095f3098075194d48fe
shakhriyor2211 <shakhriyormamadaliev@gmail.com>
2025-12-22 17:15:50 +0500

    fix

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -5,6 +5,7 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  assetPrefix: process.env.NODE_ENV === "production" ? "/docs/" : "/",
 };
 
 export default withMDX(config);
