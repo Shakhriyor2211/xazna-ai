4336f14dc1f16c803173b32e3e356b7322cf049e
shakhriyor2211 <shakhriyormamadaliev@gmail.com>
2026-01-23 18:17:27 +0500

    fix

--- client/.intlayer/fetch_dictionary/main-content.cjs
+++ client/.intlayer/fetch_dictionary/main-content.cjs
@@ -1,6 +1,6 @@
 const content = {
-  'en': () => (async () => { try {return await fetch('http://localhost:4000/dictionaries/main-content/en').then(res => res.json())} catch (_error) {return Promise.resolve(require('./../dynamic_dictionary/main-content.en.json'))}})(),
   'uz': () => (async () => { try {return await fetch('http://localhost:4000/dictionaries/main-content/uz').then(res => res.json())} catch (_error) {return Promise.resolve(require('./../dynamic_dictionary/main-content.uz.json'))}})(),
-  'ru': () => (async () => { try {return await fetch('http://localhost:4000/dictionaries/main-content/ru').then(res => res.json())} catch (_error) {return Promise.resolve(require('./../dynamic_dictionary/main-content.ru.json'))}})()
+  'ru': () => (async () => { try {return await fetch('http://localhost:4000/dictionaries/main-content/ru').then(res => res.json())} catch (_error) {return Promise.resolve(require('./../dynamic_dictionary/main-content.ru.json'))}})(),
+  'en': () => (async () => { try {return await fetch('http://localhost:4000/dictionaries/main-content/en').then(res => res.json())} catch (_error) {return Promise.resolve(require('./../dynamic_dictionary/main-content.en.json'))}})()
 };
 module.exports = content;
--- client/.intlayer/fetch_dictionary/stt-content.cjs
+++ client/.intlayer/fetch_dictionary/stt-content.cjs
@@ -1,6 +1,6 @@
 const content = {
-  'en': () => (async () => { try {return await fetch('http://localhost:4000/dictionaries/stt-content/en').then(res => res.json())} catch (_error) {return Promise.resolve(require('./../dynamic_dictionary/stt-content.en.json'))}})(),
+  'ru': () => (async () => { try {return await fetch('http://localhost:4000/dictionaries/stt-content/ru').then(res => res.json())} catch (_error) {return Promise.resolve(require('./../dynamic_dictionary/stt-content.ru.json'))}})(),
   'uz': () => (async () => { try {return await fetch('http://localhost:4000/dictionaries/stt-content/uz').then(res => res.json())} catch (_error) {return Promise.resolve(require('./../dynamic_dictionary/stt-content.uz.json'))}})(),
-  'ru': () => (async () => { try {return await fetch('http://localhost:4000/dictionaries/stt-content/ru').then(res => res.json())} catch (_error) {return Promise.resolve(require('./../dynamic_dictionary/stt-content.ru.json'))}})()
+  'en': () => (async () => { try {return await fetch('http://localhost:4000/dictionaries/stt-content/en').then(res => res.json())} catch (_error) {return Promise.resolve(require('./../dynamic_dictionary/stt-content.en.json'))}})()
 };
 module.exports = content;
